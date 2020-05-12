4d68ede697510d2b85591e09c0607c0066f85573
TrivilinDev <trivilin.dev@hotmail.com>
2020-05-12 09:03:42 -0300

    Adding Start screen to the routing

---
+++ src/pages/Start.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+export default function Start(){
+
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'red',
+        flex: 1,
+    },
+})
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Home from '~/pages/Main/Home'
+import Start from '~/pages/Start'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
@@ -10,7 +11,8 @@ const Stack = createStackNavigator();
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }} >
+            <Stack.Navigator initialRouteName='Start' screenOptions={{ headerShown: false }} >
+                <Stack.Screen name='Start' component={Start} />
                 <Stack.Screen name='Home' component={Home} />
             </Stack.Navigator>
         </NavigationContainer>
