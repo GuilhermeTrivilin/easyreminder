c71f673e60d6e33a3c4a0c06f89c64cebe93430a
TrivilinDev <trivilin.dev@hotmail.com>
2020-05-23 19:32:24 -0300

    Installing BottomTabNavigator

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ const Stack = createStackNavigator();
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Preload' screenOptions={{ headerShown: false }} >
+            <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }} >
                 <Stack.Screen name='Preload' component={Preload} />
                 <Stack.Screen name='FirstEntryHome' component={FirstEntryHome} />
                 <Stack.Screen name='FirstEntryStart' component={FirstEntryStart} />
