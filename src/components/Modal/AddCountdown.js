fe5d90d2951ba63edc3d0eebdff1a9979cca039b
TrivilinDev <trivilin.dev@hotmail.com>
2020-05-23 21:39:56 -0300

    Adding inputs to add countdown modal

--- src/components/Modal/AddCountdown.js
+++ src/components/Modal/AddCountdown.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, StyleSheet, Modal, Button } from 'react-native'
+import { View, StyleSheet, Modal, Button, Text, TouchableOpacity } from 'react-native'
 import Input from '~/components/Input'
+import { colors } from '~/commons'
 
-const AddCountdown = ({closeModal, visible}) => {
+const AddCountdown = ({ closeModal, visible }) => {
 
     return (
         <Modal
@@ -12,10 +13,28 @@ const AddCountdown = ({closeModal, visible}) => {
         >
             <View style={styles.offset}>
                 <View style={styles.container}>
+                    <Text style={[styles.redText, {fontSize: 15}]}>ADICIONAR COUNTDOWN</Text>
 
                     <Input
                         placeholder='Título do Countdown'
                     />
+
+                    <View style={styles.doubleInput}>
+                        <Input
+                            placeholder='Quantidade de dias'
+                        />
+
+                        <Text>ou</Text>
+
+                        <TouchableOpacity
+                            onPress={() => console.log('open date picker')}
+                        >
+                            <Text style={styles.redText}>SELECIONAR DATA</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <Text style={styles.text}>Selecione a cor desejada:</Text>
+
                     <Button title='fechar' onPress={closeModal} />
                 </View>
             </View>
@@ -34,6 +53,20 @@ const styles = StyleSheet.create({
         width: '95%',
         padding: 20,
         backgroundColor: '#FFF',
+    },
+    redText: {
+        fontWeight: 'bold',
+        color: colors.red,
+        textAlign: 'center',
+    },
+    doubleInput: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    text: {
+        marginVertical: 15,
+        fontSize: 15
     }
 })
 
